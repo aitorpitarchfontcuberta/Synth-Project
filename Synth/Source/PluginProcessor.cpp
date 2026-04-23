daccc9d07eb385e592c2b786dfce613ccc54585c
aitorpitarchfontcuberta <you@example.com>
2026-04-23 22:21:50 +0200

    estructura synthvoice i synthsound

--- Synth/Source/PluginProcessor.cpp
+++ Synth/Source/PluginProcessor.cpp
@@ -22,10 +22,13 @@ SynthAudioProcessor::SynthAudioProcessor()
                        )
 #endif
 {
+	synth.addSound(new SynthSound());
+	synth.addVoice(new SynthVoice());
 }
 
 SynthAudioProcessor::~SynthAudioProcessor()
 {
+    synth.setCurrentPlaybackSampleRate (getSampleRate());
 }
 
 //==============================================================================
@@ -135,27 +138,21 @@ void SynthAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
+    
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+	for (int i = 0; i < synth.getNumVoices(); ++i)
     {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
+        if (auto voice = dynamic_cast<juce::SynthesiserVoice*>(synth.getVoice(i)))
+        {
+            //osc
+            //adsr
+            //lfo
+        }
     }
+
+	synth.renderNextBlock(buffer, midiMessages, 0, buffer.getNumSamples());
 }
 
 //==============================================================================
@@ -189,3 +186,6 @@ juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new SynthAudioProcessor();
 }
+
+
+//Value Tree
